f2e67121b050b72dd205b3aa029b8cfe2a46f9a2
Roman Nigmatullin <rmnigmatullin>
2023-02-12 21:01:43 +0300

    light funny config for alignment group

--- boids/config.py
+++ boids/config.py
@@ -1,15 +1,15 @@
 w = 1920
 h = 1080
-N = 200
+N = 100
 dt = 0.1
 asp = w / h
 perception = 1 / 20
-vrange = (0, 0.1)
-coefficients = {"alignment": 0.04,
-                "cohesion": 0.02,
-                "separation": 6,
-                "walls": 0.01,
-                "noise": 0.001}
+v_range = (0, 0.1)
+coefficients = {"alignment": 0.3,
+                "cohesion": 0.5,
+                "separation": 15,
+                "walls": 0.2,
+                "noise": 0.04}
 
 video = False
-frames = 1800
+frames = 7200
